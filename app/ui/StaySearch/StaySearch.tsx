b2fb752c6d3ed5b6c1f078811491f9ba44819229
P4t4m8n <eranopenuni@gmail.com>
2024-04-25 12:12:56 +0300

    Search style

---
+++ app/ui/StaySearch/StaySearch.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent } from "react";
+import { SearchSVG } from "../svgs/svgs";
+import styles from "./StayList.module.scss";
+
+export function StaySearch() {
+  const handleSearch = (ev: ChangeEvent<HTMLInputElement>) => {
+    console.log("ev:", ev);
+  };
+
+  return (
+    <div className={styles.search}>
+      <div className={styles.inputBtn}>
+        <span>Where</span>
+        <input
+          placeholder="Search destinations"
+          value="{searchQuery}"
+          onChange={handleSearch}
+        />
+      </div>
+      <button className={styles.inputBtn}>
+        <span>Check in</span>
+        <p>Add dates</p>
+      </button>
+      <button className={styles.inputBtn}>
+        <span>Check out</span>
+        <p>Check out</p>
+      </button>
+      <button className={styles.inputBtn}>
+        <span>Who</span>
+        <p>Who</p>
+      </button>
+      <button className={styles.searchBtn}>
+        <SearchSVG />
+      </button>
+    </div>
+  );
+}
