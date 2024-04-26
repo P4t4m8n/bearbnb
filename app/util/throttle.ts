12ba2de719cf697561e3222d7bdb63405b24ac52
P4t4m8n <eranopenuni@gmail.com>
2024-04-26 12:18:19 +0300

    theottle func

---
+++ app/util/throttle.ts
@@ -0,0 +1,40 @@
+// export const throttle = <T extends (...args: any[]) => void>(
+//   func: T,
+//   timeout: number
+// ): ((...args: Parameters<T>) => void) => {
+//   let ready: boolean = true;
+//   console.log("ready:", ready)
+
+//   return (...args: Parameters<T>): void => {
+//     if (!ready) {
+//       return;
+//     }
+
+//     ready = false;
+//     func(...args);
+//     setTimeout(() => {
+//       ready = true;
+//     }, timeout);
+//   };
+// };
+
+export const throttle = <T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): ((...args: Parameters<T>) => ReturnType<T>) => {
+  let inThrottle: boolean;
+  let lastResult: ReturnType<T>;
+
+  return function (
+    this: ThisParameterType<T>,
+    ...args: Parameters<T>
+): ReturnType<T> {
+    const context = this;
+    if (!inThrottle) {
+        inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+      lastResult = func.apply(context, args);
+    }
+    return lastResult;
+  };
+};
