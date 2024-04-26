2025d3a38da326b7198402ff571fe0b53c0d4128
P4t4m8n <eranopenuni@gmail.com>
2024-04-26 12:20:41 +0300

    Add link to details

--- app/ui/StayPreview/StayPreview.tsx
+++ app/ui/StayPreview/StayPreview.tsx
@@ -3,38 +3,45 @@ import { StaySmall } from "../../model/stay.model";
 import { LikeSVG, RatingSVG } from "../svgs/svgs";
 import { faker } from "@faker-js/faker";
 import styles from "./StayPreview.module.scss";
+import Link from "next/link";
 
 interface Props {
   stay: StaySmall;
 }
 export default function StayPreview({ stay }: Props) {
-  const { rating, price, location, image } = stay;
+  const { rating, price, location, image, id } = stay;
   const { city, country } = location;
   const roundNum = Number(rating.toFixed(2));
   const distance = faker.number.int({ min: 10, max: 1200 });
 
   return (
     <li className={styles.stayPreview}>
-      <LikeSVG className={styles.likeSvg} />
-      <div className={styles.imgCon}>
-
-      <Image src={image} fill={true} alt="" className={styles.image}></Image>
-      </div>
-      <div className={styles.infoHead}>
-        <h3>
-          {city}, {country}
-        </h3>
-        <div className={styles.rating}>
-          <RatingSVG className={styles.ratingSvg} />
-          <h5>{roundNum}</h5>
+      <Link href={`stay/${id}`}>
+        <LikeSVG className={styles.likeSvg} />
+        <div className={styles.imgCon}>
+          <Image
+            src={image}
+            fill={true}
+            alt=""
+            className={styles.image}
+          ></Image>
         </div>
-      </div>
-      <h6>{distance} kilometers away</h6>
-      <h6>May 7-12</h6>
-      <div className={styles.price}>
-        <h3>{price}$</h3>
-        <h5>night</h5>
-      </div>
+        <div className={styles.infoHead}>
+          <h3>
+            {city}, {country}
+          </h3>
+          <div className={styles.rating}>
+            <RatingSVG className={styles.ratingSvg} />
+            <h5>{roundNum}</h5>
+          </div>
+        </div>
+        <h6>{distance} kilometers away</h6>
+        <h6>May 7-12</h6>
+        <div className={styles.price}>
+          <h3>{price}$</h3>
+          <h5>night</h5>
+        </div>
+      </Link>
     </li>
   );
 }
