3248b6c6b9f235a8722fc93088c407486e467c7f
P4t4m8n <eranopenuni@gmail.com>
2024-04-26 12:19:18 +0300

    Add get one

--- app/service/stay-service.tsx
+++ app/service/stay-service.tsx
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { Stay, StaySmall } from "../model/stay.model";
-import { getCache } from "./cache";
+import { getCache, setCache } from "./cache";
+import { throws } from "assert";
+import { error } from "console";
 
 const prisma = new PrismaClient();
 
@@ -31,7 +33,7 @@ export async function getSmallStays(): Promise<StaySmall[] | undefined> {
     });
 
     if (!stays) throw new Error("Unable to load");
-
+console.log('********')
     const mappedStays = stays.map(
       (stay: {
         id: any;
@@ -55,10 +57,53 @@ export async function getSmallStays(): Promise<StaySmall[] | undefined> {
             : 0,
       })
     );
-
+    await setCache(cacheKey, mappedStays);
     return mappedStays;
   } catch (error) {
     console.error("Failed to fetch stays:", error);
     return undefined;
+  }
+}
+
+export async function getStayById(stayId: string): Promise<any> {
+  console.log("stayId:", stayId);
+  try {
+    const stay = await prisma.stay.findUnique({
+      where: { id: stayId },
+      include: {
+        images: {
+          take: 5,
+          select: {
+            url: true,
+          },
+        },
+        amenities: {
+          select: {
+            name: true,
+          },
+        },
+        labels: {
+          select: {
+            name: true,
+          },
+        },
+        location: true,
+        reviews: true,
+        likes: true,
+      },
+    });
+
+    if (!stay) throw new Error("Stay not found");
+
+    stay.rating =
+      stay.reviews.length > 0
+        ? stay.reviews.reduce((acc, curr) => acc + curr.rate, 0) /
+          stay.reviews.length
+        : 0;
+
+    return stay;
+  } catch (error) {
+    console.error("Failed to retrieve stay:", error);
+    throw error;
   }
 }
